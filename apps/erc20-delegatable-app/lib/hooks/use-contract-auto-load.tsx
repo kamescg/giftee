0622b406a53a6ac530cfd500cdd55ad93d1b6970
Ryan McPeck <mcoso.dev@gmail.com>
2023-04-03 10:30:33 -0400

    fix: op re deploy

--- apps/erc20-delegatable-app/lib/hooks/use-contract-auto-load.tsx
+++ apps/erc20-delegatable-app/lib/hooks/use-contract-auto-load.tsx
@@ -17,22 +17,22 @@ export function useContractAutoLoad(contract: string, chainId?: number): any {
       switch (contract) {
         case 'ERC20Manager':
           return {
-            address: '0xD63adCa091f56694fC97e66d10085cc9192dE1Bc',
+            address: '0x2BF98a2E536903d620f71849739c6bFBa36dD929',
             abi: [],
           }
         case 'ERC20FromAllowanceEnforcer':
           return {
-            address: '0xc8a31e53e62415aAB38B5B17aa0e6b04d33dB0F1',
+            address: '0xB631Ad3c161F4714EAc4964ffb054f439a51bC53',
             abi: [],
           }
         case 'TimestampBeforeEnforcer':
           return {
-            address: '0x4510c9e7e1bcfb0c275E1AcE270E6010DB2EeD92',
+            address: '0x0e86D6bf3c7eBAa1f89c851f914be32617530579',
             abi: [],
           }
         case 'TimestampAfterEnforcer':
           return {
-            address: '0x000830BB68B17987824Cb1124334abE56cBE7E0C',
+            address: '0x752b85304642cf9e8e307e768b486e33f938df9A',
             abi: [],
           }
         case 'USDC':
